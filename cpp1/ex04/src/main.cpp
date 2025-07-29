009efcc09099ca17861328130a526926d5e90b5e
Parapau <pau.fto@gmail.com>
2025-07-29 22:51:26 +0200

    uork

--- cpp1/ex04/src/main.cpp
+++ cpp1/ex04/src/main.cpp
@@ -6,16 +6,28 @@
 /*   By: pafranco <pafranco@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 21:12:46 by pafranco          #+#    #+#             */
-/*   Updated: 2025/07/28 21:25:12 by pafranco         ###   ########.fr       */
+/*   Updated: 2025/07/29 18:34:51 by tv               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main()
+void	checkWord(std::ifstream in)
 {
-	std::ofstream MyFile("a.txt");
-	MyFile << "Puta espanya";
+}
+
+int	main()
+{
+	std::string		text;
+	std::string		fin;
+
+	std::ifstream MyFile("a.txt");
+	while (std::getline(MyFile, text))
+	{
+		std::cout << text << std::endl;
+		fin << text << std::endl;
+	}
 	MyFile.close();
 }
