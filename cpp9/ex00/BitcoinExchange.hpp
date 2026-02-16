b22df3af7f7a42d980bbd9862de1b3fc90fd972e
Parapau <pau.fto@gmail.com>
2026-02-16 17:24:02 +0100

    started work on cpp9

---
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pafranco <pafranco@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 17:17:00 by pafranco          #+#    #+#             */
+/*   Updated: 2026/02/16 17:23:05 by pafranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange
+{
+	private:
+
+}
+
+
+
+
+#endif
